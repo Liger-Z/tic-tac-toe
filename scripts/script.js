b9d6ecfd5d568be3bdd537932cb254a257cab146
Liger <jtadub@gmail.com>
2020-06-26 16:05:03 +1200

    Work on game logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,65 +1,149 @@
 // Player function factory
-const Player = (name, id) => {
-  return {name, id};
+const Player = (name, id, mark) => {
+  let turn = false;
+  return {name, id, mark, turn};
 };
 
 // Tile function factory
-const Tile = (tileNumber) => {
+const Tile = (tileNumber, div) => {
   let state = "empty";
-
-  return {tileNumber, state};
+  return {tileNumber, div, state};
 };
 
 // Game logic module
 const gameLogic = (() => {
+  let player1 = Player('', 'player1', 'O');
+  let player2 = Player('', 'player2', 'X');
+
   const playerStart = () => { // Decides who starts first
     let num = Math.floor(Math.random() * 2)
-    return num === 1 ? "player1.name" : "player2.name";
+    num === 1 ? player1.turn = true : player2.turn = true;
+    return player1.turn === true ? player1.name : player2.name;
+  };
+  const changeTurn = () => {
+    if (player1.turn === true) {
+      player1.turn = false;
+      player2.turn = true;
+    }else {
+      player1.turn = true;
+      player2.turn = false;
+    }
+  };
+
+  const gameFlow = () => {
+    gameboard.tiles.forEach(tile => {
+      tile.div.addEventListener("click", () => {
+        if (player1.turn === true) {
+          tile.div.innerText = 'O';
+          changeTurn();
+        }else {
+          tile.div.innerText = 'X';
+          changeTurn();
+        }
+      });
+    })
   };
 
   return {
     playerStart,
+    gameFlow,
+    player1,
+    player2,
   };
 })();
 
 // Gameboard module
 const gameboard = (() => {
-  const _gameboard = document.querySelectorAll(".gameboard-tile");
+  const gameboard = document.querySelectorAll(".gameboard-tile");
   const _gameboardInfo = document.querySelector(".gameboard-info");
-  const _tiles = [];
+  const tiles = [];
   for (let i = 0; i < 9; i++) {
-    _tiles.push(Tile(i));
+    tiles.push(Tile(i, gameboard[i]));
   }
 
-  let playerTurn = document.createElement("p");
-  playerTurn.classList.add("player-turn");
-  playerTurn.textContent = `It is now ${gameLogic.playerStart()}'s turn`;
-  _gameboardInfo.appendChild(playerTurn);
+  const showInitialTurn = () => {
+    let playerTurn = document.createElement("p");
+    playerTurn.classList.add("player-turn");
+    playerTurn.textContent = `It is now ${gameLogic.playerStart()}'s turn`;
+    _gameboardInfo.appendChild(playerTurn);
+  }
+
+  const showTurn = () => {
+    let
+    let playerTurn = document.createElement("p");
+    playerTurn.classList.add("player-turn");
+    
+  }
 
   return { 
+    gameboard,
+    showInitialTurn,
+    tiles,
   };
 })();
 
-const newGameButton = (() => {
+// Button module
+const button = (() => {
   const _contentWrap = document.querySelector("#content-wrap");
   const _newGameButton = document.querySelector("#new-game-button");
-  const _newGameForm = document.querySelector("#new-game-form")
-  const _gameType = document.querySelector("#game-type");
+  const _newGameForm = document.querySelector(".new-game-form")
+  const _gameType = document.querySelector(".game-type");
   const _vsPlayerButton = document.querySelector("#vs-player-button");
-  const _vsPlayer = document.querySelector("#vs-player");
+  const _vsPlayer = document.querySelector(".vs-player");
+  const _vsPlayerConfirmButton = document.querySelector("#vs-player-confirm-button");
+  const _vsPlayerForm = document.querySelector(".vs-player-form-wrap");
 
   _newGameButton.addEventListener("click", newGameForm);
   _vsPlayerButton.addEventListener("click", vsPlayer);
+  _vsPlayerConfirmButton.addEventListener("click", startPlayerGame);
+  _vsPlayerConfirmButton.addEventListener("click", gameLogic.gameFlow);
 
-  function newGameForm () {
-    _newGameForm.style.display = "flex";
+  function newGameForm() {
+    _newGameForm.classList.remove("inactive");
+    _gameType.classList.remove("inactive");
     _contentWrap.style.filter = "blur(5px)"
   };
   
   function vsPlayer() {
-    _vsPlayer.style.display = "flex";
-    _gameType.style.display = "none";
-  }
+    _gameType.classList.add("inactive");
+    _vsPlayer.classList.remove("inactive");
+  };
+
+  function startPlayerGame() {
+    event.preventDefault();
+    
+    if (checkForm() === false) { return null };
+    gameboard.showInitialTurn();
+    _vsPlayer.classList.add("inactive");
+    _contentWrap.style.filter = "";
+  };
+
+  function checkForm() {
+    const textFields = document.querySelectorAll(".input-text");
+    const radioFields = document.querySelectorAll(".input-radio");
+    let emptyField = false;
+    let checkedRadio = false;
+
+    textFields.forEach(field => {
+      if (field.value === "") { emptyField = true };
+      if (field.name === "player1" && field.value !== "") {
+        gameLogic.player1.name = `${field.value}`;
+      }else if (field.name === "player2" && field.value !== "") {
+        gameLogic.player2.name = `${field.value}`;
+      };
+    });
+
+    radioFields.forEach(field => {
+      if (field.checked === true) { checkedRadio = true };
+    });
+
+    if (emptyField === true | checkedRadio !== true) {
+      return false;
+    }else {
+      return true;
+    }
+  };
+
   return {
 
   };
